d683159b7402fa524835b6fad3588fe4209630a9
Daniel Figueira Ferreira <danielfigueira31@gmail.com>
2018-04-02 00:33:54 -0300

    Inicio para a página de login
    Criei apenas esse cabeçalho de base mesmo, talvezx depois eu comece a tentar fazer a página de login, o único problema está em como incorporar o react com várias páginas uma chamando as outras
    (espero descobrir isso amanhã)

---
+++ src/views/login/Login.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Login.css'
+
+
+class Login extends React.Component{
+    render(){
+        return (<div class = 'page'>
+
+
+
+        </div>);
+    }
+}
+
+export default Login;
